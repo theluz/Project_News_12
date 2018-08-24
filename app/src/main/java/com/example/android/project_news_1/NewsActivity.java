fa0d0fc5dad2c7c402dcf3b133674003e0908000
theluz <theluz@users.noreply.github.com>
2018-08-23 23:08:19 -0300

    Project News Parte 2
    Adicionada sessão de Prefências com 3 filtros que alteram a URL de busca

--- app/src/main/java/com/example/android/project_news_1/NewsActivity.java
+++ app/src/main/java/com/example/android/project_news_1/NewsActivity.java
@@ -5,11 +5,16 @@ import android.content.Intent;
 import android.app.LoaderManager;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -23,7 +28,9 @@ public class NewsActivity extends AppCompatActivity implements LoaderCallbacks<L
 
     private static final String LOG_TAG = NewsActivity.class.getName();
 
-    private static final String GUARDIAN_REQUEST_URL = "https://content.guardianapis.com/search?show-tags=contributor&show-fields=body-text&q=games&api-key=6b66f283-2d0a-4cec-b253-ee27a84ba59b";
+    //URL fixa anterior
+    //private static final String GUARDIAN_REQUEST_URL = "https://content.guardianapis.com/search?show-tags=contributor&show-fields=body-text&q=games&api-key=6b66f283-2d0a-4cec-b253-ee27a84ba59b";
+    private static final String GUARDIAN_REQUEST_URL = "https://content.guardianapis.com/search";
 
     private static final int NEWS_LOADER_ID = 1;
 
@@ -66,7 +73,22 @@ public class NewsActivity extends AppCompatActivity implements LoaderCallbacks<L
         }
         @Override
         public Loader<List<News>> onCreateLoader(int i, Bundle bundle){
-            return new NewsLoader(this, GUARDIAN_REQUEST_URL);
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+            String orderPreference = sharedPrefs.getString(
+                    getString(R.string.settings_order_by_key),
+                    getString(R.string.settings_order_by_default));
+
+            Uri baseUri = Uri.parse(GUARDIAN_REQUEST_URL);
+            Uri.Builder uriBuilder = baseUri.buildUpon();
+            uriBuilder.appendQueryParameter("show-tags","contributor");
+            uriBuilder.appendQueryParameter("show-fields","body-text");
+            uriBuilder.appendQueryParameter("q","games");
+            uriBuilder.appendQueryParameter("order-by",orderPreference);
+            uriBuilder.appendQueryParameter("api-key","6b66f283-2d0a-4cec-b253-ee27a84ba59b");
+
+            Log.e("######################URL Montada",uriBuilder.toString());
+            Log.e("VARIAVEL ORDERPREFERENCE = ",orderPreference);
+            return new NewsLoader(this, uriBuilder.toString());
         }
         @Override
         public void onLoadFinished(Loader<List<News>> loader, List<News> newses){
@@ -82,5 +104,21 @@ public class NewsActivity extends AppCompatActivity implements LoaderCallbacks<L
         @Override
         public void onLoaderReset(Loader<List<News>> loader){
             mAdapter.clear();
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu (Menu menu){
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+        }
+        @Override
+        public boolean onOptionsItemSelected (MenuItem item){
+            int id = item.getItemId();
+            if (id == R.id.action_settings){
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
         }
 }
---
+++ app/src/main/java/com/example/android/project_news_1/SettingsActivity.java
@@ -0,0 +1,50 @@
+package com.example.android.project_news_1;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import static android.os.Build.VERSION_CODES.O;
+
+/**
+ * Created by Luz on 19/08/2018.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+    public static class NewsPreferenceFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+
+            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSumaryToValue(orderBy);
+
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        private void bindPreferenceSumaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+    }
+}
